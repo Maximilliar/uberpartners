a6ab130f4d6a3adb053a712c7a8c3647cd4a38bd
Maksim Makarenko <80197752+Maximilliar@users.noreply.github.com>
2022-12-22 13:14:11 +0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,36 @@
+$(function () {
+    let header = $('header nav');
+    let hederHeight = header.height(); // вычисляем высоту шапки
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1) {
+            header.addClass('header_fixed');
+            $('body').css({
+                'paddingTop': hederHeight + 'px' // делаем отступ у body, равный высоте шапки
+            });
+        } else {
+            header.removeClass('header_fixed');
+            $('body').css({
+                'paddingTop': 0 // удаляю отступ у body, равный высоте шапки
+            })
+        }
+    });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
